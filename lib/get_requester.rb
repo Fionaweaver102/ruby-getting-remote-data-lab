11a82254d38485d00089dc0e39cb38465f549d3e
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-25 08:00:55 -0700

    commit tree b98a93d0305a6be3ad4e9ca0b6cd5a7164383c26

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    attr_reader :url
+     def initialize(url)
+         @url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+     end 
+
+
+
+     def get_response_body  
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body 
+     end 
+
+     def parse_json 
+        requester = JSON.parse(self.get_response_body)
+        requester.collect do |requester|
+            requester
+        end 
+     end 
+end 
